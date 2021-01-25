6e4ef9b7736eef417af8224c9eb106f1c0c32e29
Anahita246 <ana.balazadeh82@gmail.com>
2021-01-24 19:56:40 -0700

    Added starter code to the repo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# FirstRepo
+1 # FirstRepo
+2 this is a test
+3
+4 ## To compile
+5    make
+6
+7
+8 ## To run
+9
+10    ./run_this
+
---
+++ vectortests.h
@@ -0,0 +1,6 @@
+1 #include <iostream>
+2 #include <vector>
+void scalar_multiply(double scalar, std::vector<double> &d_vector);
+double vector_dot_product(std:: vector<double> first_vector,
+		std::vector<double>second_vector);
+
